47552eca7538bda8a378324f0ee0b8a0a04a3fe9
German Santos <santos.german@gmail.com>
2023-10-10 14:58:22 -0700

    Event Handling Lab

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,9 +1,18 @@
 // Code Keypad Component Here
+import React from "react";
 
-function Keypad (){
-    return (
-        <div></div>
-    )
+function Keypad() {
+  return (
+    <div>
+      <form>
+        <input
+          type="password"
+          name="password"
+          placeholder="Enter password"
+          onChange={() => console.log("Entering password...")}
+        />
+      </form>
+    </div>
+  );
 }
-
 export default Keypad;
